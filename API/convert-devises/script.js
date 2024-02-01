2d16edbf164a7de04d58862ee10aaa0120403006
Steven-Grecescu <steven.grecescu@gmail.com>
2024-02-01 16:17:54 +0100

    Commit

--- API/convert-devises/script.js
+++ API/convert-devises/script.js
@@ -10,7 +10,8 @@ async function DeviseConvertor(devise1, devise2) {
     let convertission = data.conversion_rate * val1;
     let arrondie = Math.round(convertission * 100) / 100;
 
-    document.getElementById("result").textContent =val1 +" " +devise1 +" " +"equivaut a " +arrondie +" " +devise2 +" avec un taux de change a : " +data.conversion_rate;
+    document.getElementById("result").textContent =arrondie +" " +devise2;
+    document.getElementById("taux").textContent =data.conversion_rate;
   } catch (error) {
     console.error("Erreur, devise non trouvé", error);
   }
@@ -19,5 +20,47 @@ async function DeviseConvertor(devise1, devise2) {
 function Valider() {
   const devise1 = document.getElementById("devise1").value;
   const devise2 = document.getElementById("devise2").value;
-  DeviseConvertor(devise1, devise2);
+  if (devise1 === devise2) {
+    alert("Selectionner deux devises differentes !!");
+  } else if(document.getElementById("Val1").value == "") {
+    alert("Veuillez saisir un nombre");
+  }else{
+    DeviseConvertor(devise1, devise2);
+  }
+
+
+}
+
+
+
+function ImgDeviseGauche(){
+  let modif = document.getElementById("devise1").value
+  console.log(modif);
+  document.getElementById("imgDeviseGauche").src = "img/"+ modif +".png";
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("taux").innerHTML = "";
+  document.getElementById("ValMontant").innerHTML = document.getElementById("devise1").value;
+
+}
+
+function ImgDeviseDroite(){
+  let modif = document.getElementById("devise2").value
+  console.log(modif);
+  document.getElementById("imgDeviseDroite").src = "img/"+ modif +".png";
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("taux").innerHTML = "";
+
+}
+
+function Interchanger(){
+
+  let save1 = document.getElementById("devise1").value;
+  let save2 = document.getElementById("devise2").value;
+
+  document.getElementById("devise1").value = save2;
+  document.getElementById("devise2").value = save1;
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("taux").innerHTML = "";
+  ImgDeviseGauche()
+  ImgDeviseDroite()
 }
